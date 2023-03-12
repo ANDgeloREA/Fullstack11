421cb45051dead5c4bf324f7d8e39886bf25f6c3
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-03-12 18:19:13 +0100

    complete exercise: 03

--- JavaScript/es-03/exercise-3.js
+++ JavaScript/es-03/exercise-3.js
@@ -1,19 +1,53 @@
-const sum = function (a, b) {
+//const sum = function (a, b) {
+//  return a + b;
+//}
+
+//const subtract = function (a, b) {
+//  return a - b;
+//}
+
+//const multiply = function (a, b) {
+//  return a * b;
+//}
+
+//const divide = function (a, b) {
+//  return a / b;
+//}
+
+//const log = function (value) {
+//  console.log(value);
+//}
+
+const sum = (a, b) => {
   return a + b;
 }
 
-const subtract = function (a, b) {
+const subtract = (a, b) => {
   return a - b;
 }
 
-const multiply = function (a, b) {
+const multiply = (a, b) => {
   return a * b;
 }
 
-const divide = function (a, b) {
+const divide = (a, b) => {
   return a / b;
 }
 
-const log = function (value) {
+let firstOperation = sum(2, 4);
+
+let secondOperation = sum(2, 5);
+
+let tirthOperation = multiply(firstOperation, secondOperation);
+
+let fourthOperation = subtract(tirthOperation, 2);
+
+let fifthOperation = divide(fourthOperation, 5);
+
+
+const log = (value) => {
+  value = fifthOperation;
   console.log(value);
 }
+
+log();
